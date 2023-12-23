5a99f8a0e7151e32e25c853a8422327e8eadf997
aniruddhdadhich <aniruddhddh17@gmail.com>
2023-12-23 15:32:02 +0530

    convolution in logic no equi

--- Detail.java
+++ Detail.java
@@ -1,35 +1,72 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 class Organism {
     private static int nextId = 1;
 
     private int id;
-    private double strength;
+    private double power;
     private boolean alive;
+    private int age;
     private boolean hasReproduced;
+    private Map<Integer, Integer> interactions = new HashMap<>();
 
-    public Organism(double strength) {
+    public Organism(double power) {
         this.id = nextId++;
-        this.strength = strength;
+        this.power = power;
         this.alive = true;
+        this.age = 0;
         this.hasReproduced = false;
+
     }
 
     public int getId() {
         return id;
     }
 
-    public double getStrength() {
-        return strength;
+    public double getPower() {
+        return power;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge() {
+        age++;
+    }
+
+    public boolean isMaxAge() {
+        return age >= 3;
+    }
+
+    public int getInteractions(int otherId) {
+        if (interactions.containsKey(otherId)) {
+            return interactions.get(otherId);
+        } else {
+            return 0;
+        }
+
+    }
+
+    public void setInteractions(int otherId) {
+        if (interactions.containsKey(otherId)) {
+            interactions.put(otherId, interactions.get(otherId) + 1);
+        } else {
+            interactions.put(otherId, 1);
+        }
+        // interactions.put(otherId, interactions.getOrDefault(0,
+        // interactions.get(otherId)) + 1);
     }
 
     public boolean isAlive() {
         return alive;
     }
 
-    public void eliminate() {
+    public void death() {
         alive = false;
     }
 
@@ -44,19 +81,30 @@ class Organism {
     }
 
     public void interactWith(Organism other) {
-        if (this.strength > other.strength) {
-            other.eliminate();
-        } else {
-            this.eliminate();
+        // count of interaction from both ends
+        this.setInteractions(other.getId());
+        other.setInteractions(this.getId());
+
+        // second interation
+        if (this.getInteractions(other.id) > 1) {
+            if (this.power > other.power) {
+                other.death();
+            } else {
+                this.death();
+            }
         }
+
     }
 }
 
 class Planet {
     private List<Organism> organisms;
+    // private int ageOfPlanet;
+    private int firstGen;
 
-    public Planet() {
+    public Planet(int size) {
         this.organisms = new ArrayList<>();
+        this.firstGen = size;
     }
 
     public void addOrganism(Organism organism) {
@@ -64,6 +112,34 @@ class Planet {
     }
 
     public void simulateLife() {
+
+        // pandemic logic
+        if (organisms.size() >= 20) {
+            int counter = 0;
+            // these more than 20 should be killed in pandemic
+            while (counter < organisms.size() - 20) {
+                Random random = new Random();
+                Organism orgToKill = organisms.get(random.nextInt(organisms.size()));
+                if (orgToKill.isAlive()) {
+                    counter++;
+                    organisms.remove(orgToKill);
+                }
+            }
+        }
+
+        // is eventual death
+        for (Organism organism : organisms) {
+            System.out.println("age of " + organism.getId() + " " + organism.getAge());
+            organism.setAge();
+
+            if (organism.isMaxAge()) {
+                System.out.println("death of " + organism.getAge());
+                organism.death();
+            }
+
+        }
+
+        // interaction logic
         for (Organism organism : organisms) {
             if (organism.isAlive()) {
                 Organism randomOrganism = getRandomOrganismExcept(organism);
@@ -71,19 +147,33 @@ class Planet {
                     organism.interactWith(randomOrganism);
                 }
             }
+
         }
 
-        for (Organism organism : new ArrayList<>(organisms)) {
-            if (organism.isAlive() && !organism.hasReproduced()) {
-                Organism newOrganism = new Organism(organism.getStrength());
+        // reprod logic
+        int reprodCounter = 0;
+        while (reprodCounter <= 4) {
+            Random random = new Random();
+            Organism parentOrg = organisms.get(random.nextInt(organisms.size()));
+
+            if (parentOrg.isAlive() && !parentOrg.hasReproduced()) {
+                Organism newOrganism = new Organism(parentOrg.getPower());
                 organisms.add(newOrganism);
-                organism.reproduce();
+                parentOrg.reproduce();
+                reprodCounter++;
             }
         }
+
     }
 
     private Organism getRandomOrganismExcept(Organism excludeOrganism) {
-        List<Organism> validOrganisms = new ArrayList<>(organisms);
+        List<Organism> validOrganisms = new ArrayList<>();
+        for (Organism org : organisms) {
+            if (org.isAlive()) {
+                validOrganisms.add(org);
+            }
+        }
+
         validOrganisms.remove(excludeOrganism);
 
         if (validOrganisms.isEmpty()) {
@@ -104,4 +194,3 @@ class Planet {
         return livingOrganisms == organisms.size() / 2;
     }
 }
-
--- LifeSimulation.java
+++ LifeSimulation.java
@@ -1,17 +1,18 @@
 public class LifeSimulation {
     public static void main(String[] args) {
-        Planet planet = new Planet();
+        int firstGen = 7;
+        Planet planet = new Planet(firstGen);
 
         // Assumption: Populate the planet with 10 organisms initially
-        for (int i = 0; i < 10; i++) {
-            double randomStrength = Math.random() * 100;
-            Organism organism = new Organism(randomStrength);
+        for (int i = 0; i < firstGen; i++) {
+            double randomPower = Math.random() * 100;
+            Organism organism = new Organism(randomPower);
             planet.addOrganism(organism);
         }
 
         // Simulation loop
         while (!planet.isEquilibrium()) {
-            planet.simulateLife();
+            planet.simulateLife(); 
         }
 
         System.out.println("Equilibrium reached. Life on the planet is at peace and sustainable.");
